b8902bf34a9ddc47f5fee71b4087f7a72a74e272
alirezafazeli8 <alirezafazeli34@gmail.com>
2024-10-16 23:11:40 +0330

    exam 1 is completed

---
+++ مباحث ویژه/exam-1/main.py
@@ -0,0 +1,17 @@
+num_one = 2
+num_two = 3
+
+# +
+print(num_one + num_two)
+# -
+print(num_one - num_two)
+# *
+print(num_one * num_two)
+# /
+print(num_one / num_two)
+# **
+print(num_one**num_two)
+# %
+print(num_one % num_two)
+# //
+print(num_one // num_two)
